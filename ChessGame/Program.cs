b4d34c44b65dfaf044b1e8b08c66a41ebf1fdb65
victorguirra <victor.hugo.guirra@gmail.com>
2023-03-28 11:54:30 -0300

    Criacao da classe PosicaoXadrez

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,19 +1,5 @@
-﻿using ChessGame;
-using ChessGame.tabuleiro;
-using ChessGame.xadrez;
+﻿using ChessGame.xadrez;
 
-try
-{
-    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-    tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-    tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-    tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
-    tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
-    
-    Tela.ImprimirTabuleiro(tabuleiro);
-}
-catch(TabuleiroException ex)
-{
-    Console.WriteLine(ex.Message);
-}
+PosicaoXadrez posicao = new PosicaoXadrez('a', 1);
+Console.WriteLine(posicao);
+Console.WriteLine(posicao.ToPosicao());
