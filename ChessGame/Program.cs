9dc4025fb8d8e9de694b8b792f290c6724880ec9
victorguirra <victor.hugo.guirra@gmail.com>
2023-03-30 13:18:52 -0300

    Implementacao de restricao de movimentos

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -8,24 +8,39 @@ try
 
     while (!partidaXadrez.Terminada)
     {
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partidaXadrez.Tabuleiro);
+        try
+        {
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partidaXadrez.Tabuleiro);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine($"Turno: {partidaXadrez.Turno}");
+            Console.WriteLine($"Aguardando jogada: {partidaXadrez.JogadorAtual}");
 
-        Console.WriteLine(string.Empty);
-        Console.Write("Origem: ");
-        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            Console.WriteLine(string.Empty);
+            Console.Write("Origem: ");
+            Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            partidaXadrez.ValidarPosicaoOrigem(origem);
 
-        bool[,] posicoesPossiveis = partidaXadrez.Tabuleiro.Peca(origem).MovimentosPossiveis();
+            bool[,] posicoesPossiveis = partidaXadrez.Tabuleiro.Peca(origem).MovimentosPossiveis();
 
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partidaXadrez.Tabuleiro, posicoesPossiveis);
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partidaXadrez.Tabuleiro, posicoesPossiveis);
 
-        Console.WriteLine(string.Empty);
-        Console.Write("Destino: ");
-        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+            Console.WriteLine(string.Empty);
+            Console.Write("Destino: ");
+            Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+            partidaXadrez.ValidarPosicaoDestino(origem, destino);
 
-        partidaXadrez.ExecutaMovimento(origem, destino);
+            partidaXadrez.RealizaJogada(origem, destino);
+        }
+        catch(TabuleiroException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Pressione Enter para refazer a jogada!");
+            Console.ReadLine();
+        }
     }
+        
 }
 catch(TabuleiroException ex)
 {
--- ChessGame/xadrez/PartidaXadrez.cs
+++ ChessGame/xadrez/PartidaXadrez.cs
@@ -5,8 +5,8 @@ namespace ChessGame.xadrez
     internal class PartidaXadrez
     {
         public Tabuleiro Tabuleiro { get; set; }
-        private int Turno;
-        private Cor JogadorAtual;
+        public int Turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
 
         public PartidaXadrez()
@@ -25,6 +25,43 @@ namespace ChessGame.xadrez
 
             Peca pecaCapturada = Tabuleiro.RetirarPeca(destino);
             Tabuleiro.ColocarPeca(peca, destino);
+        }
+
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            Turno++;
+            MudaJogador();
+        }
+
+        public void ValidarPosicaoOrigem(Posicao pos)
+        {
+            Peca peca = Tabuleiro.Peca(pos);
+
+            if (peca == null)
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+
+            if (JogadorAtual != peca.Cor)
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+
+            if(!peca.ExisteMovimentosPossiveis())
+                throw new TabuleiroException("Não há movimentos possíveis para a peça de origem escolhida!");
+        }
+
+        public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
+        {
+            Peca pecaOrigem = Tabuleiro.Peca(origem);
+
+            if (!pecaOrigem.PodeMoverPara(destino))
+                throw new TabuleiroException("Posição de destino inválida!");
+        }
+
+        private void MudaJogador()
+        {
+            if (JogadorAtual == Cor.Branca)
+                JogadorAtual = Cor.Preta;
+            else
+                JogadorAtual = Cor.Branca;
         }
 
         private void ColocarPecas()
